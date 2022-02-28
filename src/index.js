f852bdc99b7ca484fa32701914fa61527199d127
Adriana Mota <adrianamota17@hotmail.com>
2022-02-28 13:11:06 +0100

    interface

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './css/index.css'
+
+const links = document.querySelectorAll('nav a')
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    document.querySelector('nav a.active').classList.remove('active')
+    link.classList.add('active')
+
+    document.querySelectorAll('section').forEach((section) => section.id == link.href.split('#')[1] ? section.classList.add('active') : section.classList.remove('active'))
+  })
+})
